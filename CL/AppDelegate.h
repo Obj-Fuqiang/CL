75acca9003bfb5cede73bae0178c7a3e618a14c3
Love-CL <fuqiang.niu@gmail.com>
2015-11-19 21:35:13 +0800

    Initial Commit

---
+++ CL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CL
+//
+//  Created by 牛富强 on 15-11-19.
+//  Copyright (c) 2015年 牛富强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
